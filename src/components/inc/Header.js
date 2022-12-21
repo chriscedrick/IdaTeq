1f88bbfba34148070ffcac1f5b2d6e906bdc3e78
Mugisho Lwango Christian <chriscedrick4@gmail.com>
2022-12-21 16:26:56 +0200

    first commit

--- src/components/inc/Header.js
+++ src/components/inc/Header.js
@@ -1,38 +1,38 @@
-import React from "react";
-import Idalogo from '../images/Idalogo1.jpg'
+/* This example requires Tailwind CSS v3.0+ */
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from "react-router-dom";
+import Idalogo from '../images/Idalogo1.jpg'
+
 
 const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-    return(
-        <div className="bg-white">
+  return (
+    <div className="relative bg-white">
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-20">
-        <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <button id="dropdownDefault" data-dropdown-toggle="navbar-dropdown" type="button"
-                    className="inline-flex items-center justify-center rounded-md p-2 text-blue-800 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white mt-5"
-                    aria-controls="navbar-dropdown" aria-expanded="true">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+      <div className="px-6 pt-6 lg:px-8">
+        <div>
+          <nav className="flex h-9 items-center justify-between" aria-label="Global">
+            <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img src={Idalogo} className="block w-60" alt="..."/>
+                </a>
             </div>
-
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                    <img src={Idalogo} className="block w-60 mt-4" alt="..."/>
-                </div>
-
-                <div className="hidden sm:ml-auto sm:block mt-7">
-                    <div className="flex space-x-4">
-                        <Link to="/" className="text-blue-800 hover:text-blue-700 py-2 font-medium"
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:min-w-0 lg:justify-center lg:gap-x-5 lg:ml-60">
+                      <Link to="/" className="text-blue-800 hover:text-blue-700 py-2 font-medium"
                             aria-current="page">Home</Link>
 
                         <Link to="/services" className="text-blue-800 hover:text-blue-700 py-2 font-medium">IT
@@ -65,36 +65,45 @@ const Header = () => {
                                     className="text-blue-800 hover:text-blue-700 font-medium hover:bg-indigo-50">SCPIOT</Link>
                             </div>
                         </div>
+                        
 
                         <Link to="/projects" className="text-blue-800 hover:text-blue-700 py-2 font-medium">Projects</Link>
 
                         <Link to="/contact"
                             className="text-blue-800 hover:text-blue-700 pr-8 py-2 font-medium">Contact Us</Link>
-                    </div>
-                </div>
             </div>
-
-        </div>
-    </div>
-
-    {/* Mobile menu, show/hide based on menu state. */}
-    <div className="hidden" id="navbar-dropdown">
-        <div className="space-y-1 px-2 pt-2 pb-3" aria-labelledby="dropdownDefault">
-            <Link to="/"
-                className="text-blue-800 hover:text-blue-700 text-center block px-3 py-2 rounded-md text-base font-medium"
+          </nav>
+          <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+            <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+              <div className="flex h-9 items-center justify-between">
+                <div className="flex">
+                
+                <img src={Idalogo} className="block w-60 mt-4" alt="..."/>
+            
+                </div>
+                <div className="flex">
+                  <button
+                    type="button"
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                  <Link to="/"
+                className="text-blue-800 hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium"
                 aria-current="page">Home</Link>
-
-            <Link to="/services"
-                className="text-blue-800 hover:text-blue-700 text-center block px-3 py-2 rounded-md text-base font-medium">IT
+                  <Link to="/services"
+                className="text-blue-800 hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">IT
                 Services</Link>
-
-           {/* <Link to="#"
-                className="text-blue-800 hover:text-blue-700 text-center block px-3 py-2 rounded-md text-base font-medium">Programs</Link>
-            */}
-
-            <div className="dropdown">
+              <div className="dropdown">
                 <button
-                    className="dropbtn text-blue-800 hover:text-blue-700 text-center  px-[200px] py-2 rounded-md text-base font-medium inline-flex items-center">Programs
+                    className="dropbtn text-blue-800 hover:text-blue-700 px-[200px] py-2 rounded-md text-base font-medium inline-flex items-center">Programs
                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20">
                     <path
@@ -111,15 +120,22 @@ const Header = () => {
                 </div>
             </div>
 
-            <Link to="/projects" className="text-blue-800 hover:text-blue-700 text-center block px-3 py-2 rounded-md text-base font-medium">Projects</Link>
 
+            <Link to="/projects" className="text-blue-800 hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">Projects</Link>
+            
             <Link to="/contact"
-                className="text-blue-800 hover:text-blue-700 text-center block px-3 py-2 rounded-md text-base font-medium">Contact
+                className="text-blue-800 hover:text-blue-700 block px-3 py-2 rounded-md text-base font-medium">Contact
                 US</Link>
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
         </div>
+      </div>
+      </div>
     </div>
-</div>
-    );
+  );
 };
 
 export default Header;
